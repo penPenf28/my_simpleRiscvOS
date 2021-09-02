de59171f41589f6ee3b0ed60a27d08b732c24348
penpenf28 <penpenf28@163.com>
2021-09-01 21:35:25 -0700

    softwareTimer

--- user.c
+++ user.c
@@ -2,24 +2,44 @@
 
 #define DELAY 4000
 
-#define USE_LOCK
+struct userdata {
+	int counter;
+	char *str;
+};
+
+/* Jack must be global */
+struct userdata person = {0, "XHW"};
+
+void timer_func(void *arg)
+{
+	if (NULL == arg)
+		return;
+
+	struct userdata *param = (struct userdata *)arg;
+
+	param->counter++;
+	printf("======> TIMEOUT: %s: %d\n", param->str, param->counter);
+}
 
 void user_task0(void)
 {
 	uart_puts("Task 0: Created!\n");
+
+	struct timer *t1 = timer_create(timer_func, &person, 3);
+	if (NULL == t1) {
+		printf("timer_create() failed!\n");
+	}
+	struct timer *t2 = timer_create(timer_func, &person, 5);
+	if (NULL == t2) {
+		printf("timer_create() failed!\n");
+	}
+	struct timer *t3 = timer_create(timer_func, &person, 7);
+	if (NULL == t3) {
+		printf("timer_create() failed!\n");
+	}
 	while (1) {
-#ifdef USE_LOCK
-		spin_lock();
-#endif
-		uart_puts("Task 0: Begin ... \n");
-		for (int i = 0; i < 5; i++) {
-			uart_puts("Task 0: Running... \n");
-			task_delay(DELAY);
-		}
-		uart_puts("Task 0: End ... \n");
-#ifdef USE_LOCK
-		spin_unlock();
-#endif
+		uart_puts("Task 0: Running... \n");
+		task_delay(DELAY);
 	}
 }
 
@@ -27,12 +47,8 @@ void user_task1(void)
 {
 	uart_puts("Task 1: Created!\n");
 	while (1) {
-		uart_puts("Task 1: Begin ... \n");
-		for (int i = 0; i < 5; i++) {
-			uart_puts("Task 1: Running... \n");
-			task_delay(DELAY);
-		}
-		uart_puts("Task 1: End ... \n");
+		uart_puts("Task 1: Running... \n");
+		task_delay(DELAY);
 	}
 }
 
